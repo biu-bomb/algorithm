00248e0cd3b82e5bf6062c54e87660d583692f3a
unknown <wenlong.guo@zmdesk.com>
2021-06-02 19:55:24 +0800

    a

--- src/com/godme/leetcode/q105/Solution.java
+++ src/com/godme/leetcode/q105/Solution.java
@@ -4,28 +4,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-    private Map<Integer, Integer> inorderIndexMap;
-    int[] preorder, inorder;
+    private Map<Integer, Integer> indexMap;
+    int[] preorder;
+    int[] inorder;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         this.preorder = preorder;
         this.inorder = inorder;
-        for(int i = 0; i < inorder.length; i++){
-            inorderIndexMap.put(inorder[i], i);
+        int n = preorder.length;
+        // 快速定位，免查找
+        indexMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
         }
-        return buildTree(0, preorder.length-1, 0, inorder.length-1);
+        return myBuildTree(0, n - 1, 0, n - 1);
     }
-
-    public TreeNode buildTree(int preLeft, int preRight, int inLeft, int inRight){
-        if(preLeft > preRight || inLeft > inRight) return null;
-        int rootValue = preorder[preLeft];
-        int nextRootInorderIndex = inorderIndexMap.get(preorder[preLeft]);
-        int leftLength = nextRootInorderIndex - inLeft;
-        return new TreeNode(rootValue,
-                buildTree(preLeft+1, preLeft+leftLength, inLeft, inLeft+leftLength),
-                buildTree(preLeft+leftLength+1, preRight, inLeft+leftLength+1, inRight));
+    // 元素区间分割，层级递归
+    public TreeNode myBuildTree(int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+        if (preorder_left > preorder_right) return null;
+        int inorder_root = indexMap.get(preorder[preorder_left]);
+        TreeNode root = new TreeNode(preorder[preorder_left]);
+        int left_length = inorder_root - inorder_left;
+        root.left = myBuildTree(preorder_left + 1, preorder_left + left_length, inorder_left, inorder_root - 1);
+        root.right = myBuildTree(preorder_left + left_length + 1, preorder_right, inorder_root + 1, inorder_right);
+        return root;
     }
 }
-
 class TreeNode {
     int val;
     TreeNode left;
